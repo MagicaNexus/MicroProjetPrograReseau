ed107ae8272317c806e79a7ed868182a4239cf2a
Unknown <matthieurode@yahoo.fr>
2017-12-15 10:22:08 +0100

    Création et écriture des classes Authentification et AccepterClient
    Cette classe nous permettra de savoir si on a affaire à un client ou à un esclave via une connexion login password... Il faut implémenter la redirection vers leur interface, il faudra également (voir fichier XML) créer une structure DOM pour avoir une liste de maitre et d'esclave (fonctionnalité supplémentaire).
    Accepter client est un Runnable qui permet de connecter plusieurs esclaves/maitre en même temps

--- src/Classe_Client/AccepterClient.java
+++ src/Classe_Client/AccepterClient.java
@@ -1,9 +1,31 @@
 package Classe_Client;
 
-public class AccepterClient {
+import java.io.IOException;
+import java.net.*;
 
-	public AccepterClient() {
-		// TODO Auto-generated constructor stub
+class Accepterclients implements Runnable {
+
+	   private ServerSocket socketserver;
+	   private Socket socket;
+	   private int nbrclient = 1;
+		public Accepterclients(ServerSocket s){
+			socketserver = s;
+		}
+		
+		public void run() {
+
+	        try {
+	        	while(true){
+	        		  socket = socketserver.accept(); // Un client se connecte on l'accepte => Pas d'identification encore
+	                  System.out.println("Le client numéro "+nbrclient+ " est connecté !");
+	                  nbrclient++;
+	                  socket.close();
+	                  
+	        	}
+	        
+	        } catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+//Documentation de la classe File : https://docs.oracle.com/javase/7/docs/api/java/io/File.html
 public class Main {
 
 	public Main() {
