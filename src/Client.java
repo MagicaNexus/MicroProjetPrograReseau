22dacb121ea212809a003d01b4ed5c52de0a020d
Unknown <matthieurode@yahoo.fr>
2017-12-15 08:52:56 +0100

    Code Client et Serveur
    Implémentation du code Client Serveur pris sur OpenClassRoom et à élaborer

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+//Client de base pris sur OpenClassRoom
 
 public class Client {
+	
+	public static void main(String[] zero) {
+		
+		Socket socket;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+		
+		     socket = new Socket(InetAddress.getLocalHost(),2008);	
+	             socket.close();
 
+		}catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		}catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/Serveur.java
+++ src/Serveur.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//Serveur de base pris sur OpenClassRoom
 
 public class Serveur {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] zero) {
+		
+		ServerSocket socketserver  ;
+		Socket socketduserveur ;
 
+		try {
+		
+			socketserver = new ServerSocket(2008);
+			socketduserveur = socketserver.accept(); 
+			System.out.println("Nouvelle connexion");
+		        socketserver.close();
+		        socketduserveur.close();
+
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
+
