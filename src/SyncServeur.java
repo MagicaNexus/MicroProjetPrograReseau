ba70b1337f9d4cecc6073ac2188b162cf0257dfc
Unknown <matthieurode@yahoo.fr>
2017-12-19 10:17:56 +0100

    Ajout des méthodes de connexions des clients
    Mise en place d'un compteur, d'une incrémentation et décrémentation et d'un construction calculant le nombre de clients connectés

--- src/SyncServeur.java
+++ src/SyncServeur.java
@@ -6,7 +6,39 @@ import Classe_Client.*;
 //Serveur de base pris sur OpenClassRoom
 
 public class SyncServeur {
-
+	
+	static private int compteur = 0; //Compeut pour compter le nombre de clients connectés
+	private Socket clientSocket;
+	
+	
+	//méthode pour avoir le compteur
+	synchronized public int getCompteur()
+	{
+		return compteur;
+	}
+	
+	//methode pour incrémenter le compteur lorsqu'un client se connecte
+	synchronized public void incrementation()
+	{
+		compteur++;
+	}
+	
+	//methode pour décrémenter le compteur lorsqu'un client se déconnecte
+	synchronized public void décrémentation()
+	{
+		compteur--;
+	}
+	
+	
+	//Constructeur calculant le nombre de clients connectés
+	public SyncServeur(Socket clientSocket)
+	{
+		this.clientSocket = clientSocket;
+		incrementation();
+		System.out.println(getCompteur() + " clients connectés");
+	}
+	
+	//Connexion du serveur
 	public static void main(String[] args) {
 			
 			ServerSocket socket;
